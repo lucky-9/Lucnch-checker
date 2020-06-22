618f62ada5b275476a5cb7af3fffb62035d60071
lucky-9 <pavankalyanb313@gmail.com>
2020-06-22 19:20:19 +0530

    added functionality

---
+++ app.js
@@ -0,0 +1,39 @@
+(function () {
+  angular
+    .module("lunchCheker", [])
+    .controller("lunchCheckContoller", lunchCheckContoller);
+
+  function lunchCheckContoller($scope) {
+    $scope.lunchItems = "";
+    $scope.name = "pavan";
+    $scope.message = "";
+    $scope.sign = "";
+    $scope.borderStyle = "";
+    $scope.checkLunch = function () {
+      if ($scope.lunchItems == "") {
+        $scope.message = "Please enter Data first!";
+        $scope.sign = "danger";
+        $scope.borderStyle = "danger-border";
+      } else {
+        let itemsArray = $scope.lunchItems.split(",");
+        if (itemsArray.includes(" ")) {
+          $scope.warning =
+            "WARNING: Please make sure to remove , , else it would be counted!";
+        }
+
+        itemsArray.length <= 3
+          ? (($scope.message = "Enjoy!"),
+            ($scope.sign = "success"),
+            ($scope.borderStyle = "success-border"))
+          : (($scope.message = "Too much!"),
+            ($scope.sign = "danger"),
+            ($scope.borderStyle = "danger-border"));
+      }
+    };
+    $scope.onLunchItemsChange = function () {
+      $scope.message = "";
+      $scope.borderStyle = "";
+      $scope.warning = "";
+    };
+  }
+})();
